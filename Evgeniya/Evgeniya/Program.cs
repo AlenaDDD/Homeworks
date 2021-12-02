ff4786d85811c0e1a7d850bcf7b6fb3917ffee15
Degtyareva Alena <alionchikd2000@mail.ru>
2021-12-02 18:41:20 +0400

    Привет

---
+++ Evgeniya/Evgeniya/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Evgeniya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int x = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                x += arr[i]; 
+            }
+                Console.Write(x);
+        }
+    }
+}
+
